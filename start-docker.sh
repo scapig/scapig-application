bcc058f4bc515423573f8e6618e6fa8edd4b8887
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2018-02-03 21:12:54 +0000

    allow configurable parameter

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT=$(find . -type f -name scapig-application)
 rm -f scapig-application*/RUNNING_PID
-exec $SCRIPT -Dhttp.port=9012 -J-Xms16M -J-Xmx64m
+exec $SCRIPT -Dhttp.port=9012 $JAVA_OPTS -J-Xms16M -J-Xmx64m
